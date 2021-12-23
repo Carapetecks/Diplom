4207c1b1955cbbef101c1790ebc299fcf5ddd17c
Carapetecks <cool.pochta87@gmail.com>
2021-12-23 21:48:47 +1000

    testMerge

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,4 +6,23 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+    public GameObject inventory;
+    private bool inventoryOn;
+    public void Start()
+    {
+        inventoryOn = false;
+    }
+    public void Chest()
+    {
+        if (inventoryOn == false)
+        {
+            inventoryOn = true;
+            inventory.SetActive(true);
+        }
+        else if (inventoryOn == true)
+        {
+            inventoryOn = false;
+            inventory.SetActive(false);
+        }
+    }
 }
